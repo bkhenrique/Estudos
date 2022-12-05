e7c0d6d28e0501f5014995fcdc4fe3855dccf394
Henrique Bk <bksolucoesdigitais@gmail.com>
2022-12-05 00:22:04 +0000

    att java e lodalt

---
+++ estudo-html/js/javascript.js
@@ -0,0 +1,12 @@
+alert("Bem Vindo  ")
+console.log("Ola Mundo")
+// // variaveis 
+// string é um texto; nunber é um numero; bolean falso ou verdadeiro;
+let nome ="Henrique" //string
+let idade=90 //number
+let logado = true //bolean
+let deslogado = false //bolean
+let lista = ("cafe", "leite", "açucar") //object
+let nomeCompleto = {nome:"Henrique", idade:90}
+//comando abaixo exibe oo nome no console
+console.log(typeof nome )
