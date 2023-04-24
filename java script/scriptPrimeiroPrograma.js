12dcef194b2dc316ddb4b4154c0d58785041f4c1
HENRIQUE GUIMARAES <118493830+bkhenrique@users.noreply.github.com>
2023-04-24 11:09:06 +0100

    js

---
+++ java script/scriptPrimeiroPrograma.js
@@ -0,0 +1,28 @@
+;let idade = prompt('Qual sua idade?');
+
+if(idade < 18 ){
+    console.log("você é uma Criança")
+    console.log(idade)
+}else if(idade >= 18 && idade <60){
+    console.log("você é um Adulto")
+    console.log(idade)
+}else if(idade >= 60 ){
+    console.log("você é um Idoso")
+    console.log(idade)
+}
+// condicional
+// < menor q 
+// > maior q
+// == igual
+// >= maior ou igual
+// <= menor ou igual
+// != diferente
+
+// =  operador de atribuiçao
+// ==  igual a ou equal to
+// === valor e tipo igual
+
+//multiplas condiçoes
+// && (e) duas verdadeiras
+// || (ou) uma das duas verdadeiras
+//
